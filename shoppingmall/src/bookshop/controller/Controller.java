9e60da7b6f70c72e48314876aaa628176df6fa42
SeungCheol LEE <treatyouB@gmail.com>
2017-04-26 16:23:40 +0900

    add2

---
+++ shoppingmall/src/bookshop/controller/Controller.java
@@ -0,0 +1,28 @@
+package bookshop.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/Controller", "*.do" }, initParams = {
+		@WebInitParam(name = "propertyConfig", value = "commandMapping.properties") })
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Controller() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}
